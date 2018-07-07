0b3f052338802631ffd95ebeee3f5b18d007b60e
osblouf <osblouf@gmail.com>
2018-07-07 22:14:43 +0200

    Base version.

--- TDNN_layer.py
+++ TDNN_layer.py
@@ -10,6 +10,7 @@ The code presented in this repo is an implementation of Peddinti1's paper
 
 from keras import backend as K
 from keras.engine.base_layer import Layer
+from keras import activations
 import numpy as np
 
 
@@ -26,19 +27,30 @@ class TDNNLayer(Layer):
     def __init__(self,
                  input_context=[-2, 2],
                  sub_sampling=False,
+                 initializer='uniform',
+                 activation=None,
                  **kwargs):
 
-        self.output_dim = output_dim
         self.input_context = input_context
         self.sub_sampling = sub_sampling
+        self.initializer = initializer
+        self.activation = activations.get(activation)
         super(TDNNLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        # Create a trainable weight variable for this layer.
+
+        kernel_shape = (self.input_context[1]-self.input_context[0]+1,1)
         self.kernel = self.add_weight(name='kernel',
-                                      shape=(input_shape[1], self.output_dim),
-                                      initializer='uniform',
+                                      shape=kernel_shape,
+                                      initializer=self.initializer,
                                       trainable=True)
+        self.mask = np.zeros(kernel_shape)
+        self.mask[0][0] = 1
+        self.mask[self.input_context[1]-self.input_context[0]][0] = 1
+
+        if self.sub_sampling:
+            self.kernel = self.kernel * self.mask
+
         super(TDNNLayer, self).build(input_shape)  # Be sure to call this at the end
 
     def call(self,
@@ -47,7 +59,21 @@ class TDNNLayer(Layer):
              training=None,
              initial_state=None,
              constants=None):
-        return K.dot(inputs, self.kernel)
+        if self.sub_sampling:
+            output = K.conv1D(inputs,
+                              self.kernel,
+                              stride=1,
+                              padding=0,
+                              )
+        else:
+            output = K.conv1D(inputs,
+                              self.kernel * self.mask,
+                              stride=1,
+                              padding=0,
+                              )
+        if self.activation is not None:
+            return self.activation(output)
+        return output
 
     def compute_output_shape(self, input_shape):
         return input_shape[0], input_shape[1]-self.input_context[1]+self.input_context[0]
